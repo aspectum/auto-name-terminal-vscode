ab3c527b09cf22d4c7e77d7ee92ab0af7f35be73
Pedro Garcia <drogpe@gmail.com>
2022-03-06 19:00:59 -0300

    It works

--- src/extension.ts
+++ src/extension.ts
@@ -1,16 +1,25 @@
 import * as vscode from "vscode";
+import path from "path";
+import fs from "fs";
 
 export function activate(context: vscode.ExtensionContext) {
-  console.log(
-    'Congratulations, your extension "autorenameterminal" is now active!'
-  );
+  console.log("Auto Name Terminal extension active");
 
   let disposable = vscode.commands.registerCommand(
-    "autorenameterminal.helloWorld",
-    () => {
-      vscode.window.showInformationMessage(
-        "Hello World from AutoRenameTerminal!"
-      );
+    "autonameterminal.createTerminal",
+    (target) => {
+      if (target.scheme && target.scheme === "file") {
+        let p = path.resolve(target._fsPath);
+
+        if (fs.lstatSync(target._fsPath).isFile()) {
+          p = path.resolve(p, "../");
+        }
+
+        const name = p.split("\\").pop();
+
+        vscode.window.createTerminal({ name: name, cwd: p });
+        vscode.window.terminals[vscode.window.terminals.length - 1].show(false);
+      }
     }
   );
 
